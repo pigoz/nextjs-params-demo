ecb9ce370f96710a4c5aaef4564368cf7546c89e
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2024-08-11 09:10:41 +0200

    add example

---
+++ utils/url.ts
@@ -0,0 +1,31 @@
+export function structFromURLSearchParams(
+  usp: URLSearchParams,
+  options?: { parseNumbers?: true },
+) {
+  type TypeOrArray<T> = T | T[];
+  const params: Record<string, TypeOrArray<string | number | null>> = {};
+
+  function NumberParser(value: string | null) {
+    if (value === null) {
+      return null;
+    }
+    const parsed = Number(value);
+    return isNaN(parsed) ? value : parsed;
+  }
+
+  function IdentityParser(value: string | null) {
+    return value;
+  }
+
+  const parser = options?.parseNumbers ? NumberParser : IdentityParser;
+
+  for (const key of usp.keys()) {
+    if (key.endsWith("[]")) {
+      params[key.replace("[]", "")] = usp.getAll(key).map(parser);
+    } else {
+      params[key] = parser(usp.get(key));
+    }
+  }
+
+  return params;
+}
