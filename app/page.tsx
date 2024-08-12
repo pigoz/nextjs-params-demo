8a3b6cc11f6365792b9b557b8abc033c4ecc17cf
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2024-08-12 10:25:15 +0200

    add schema based solution

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
 import { PageProps, URLSearchParamsFromPageProps } from "@/utils/next";
-import { parse } from "@/utils/query-string";
+import { parseUnsafe, parseParamUnsafe } from "@/utils/query-string";
+import * as S from "@effect/schema/Schema";
+
+const SearchParams = S.struct({
+  bounds: S.tuple(
+    S.NumberFromString,
+    S.NumberFromString,
+    S.NumberFromString,
+    S.NumberFromString,
+  ),
+  foo: S.array(S.string),
+  a: S.NumberFromString,
+  b: S.string,
+});
 
 export default function Home(props: PageProps) {
   const params = URLSearchParamsFromPageProps(props);
-  const struct = parse(params, { parseNumbers: true });
+  const struct = parseUnsafe(params, SearchParams);
+  const b = parseParamUnsafe(params, "b", S.string);
+
   return (
     <main>
+      <p>parseUnsafe</p>
       <pre>{JSON.stringify(struct, null, 2)}</pre>
+
+      <p>parseParamUnsafe</p>
+      <pre>{JSON.stringify(b, null, 2)}</pre>
     </main>
   );
 }
--- utils/query-string.ts
+++ utils/query-string.ts
@@ -1,26 +1,58 @@
-export function parse(usp: URLSearchParams, options?: { parseNumbers?: true }) {
-  type TypeOrArray<T> = T | T[];
-  const params: Record<string, TypeOrArray<string | number | null>> = {};
+import { Either } from "effect";
+import * as S from "@effect/schema/Schema";
+import { ParseError } from "@effect/schema/ParseResult";
 
-  function NumberParser(value: string | null) {
-    if (value === null) {
-      return null;
-    }
-    const parsed = Number(value);
-    return isNaN(parsed) ? value : parsed;
-  }
+/*
+ * Parses a single parameter from an URLSearchParams object
+ */
+export function parseParam<K extends string, I, O>(
+  usp: URLSearchParams,
+  param: K,
+  schema: S.Schema<I, O>,
+): Either.Either<ParseError, O> {
+  return parse(usp, S.struct({ [param]: schema })).pipe(
+    Either.map((result) => result[param]),
+  );
+}
 
-  function IdentityParser(value: string | null) {
-    return value;
-  }
+/*
+ * Parses a single parameter from an URLSearchParams object.
+ * Raises ParseError if the parsing fails.
+ */
+export function parseParamUnsafe<K extends string, I, O>(
+  usp: URLSearchParams,
+  param: K,
+  schema: S.Schema<I, O>,
+): O {
+  return parseUnsafe(usp, S.struct({ [param]: schema }))[param];
+}
 
-  const parser = options?.parseNumbers ? NumberParser : IdentityParser;
+/*
+ * Parses multiple parameters from an URLSearchParams object.
+ */
+export function parse<I, O>(usp: URLSearchParams, schema: S.Schema<I, O>) {
+  return S.parseEither(schema)(parse_(usp));
+}
+
+/*
+ * Parses multiple parameters from an URLSearchParams object.
+ * Raises ParseError if the parsing fails.
+ */
+export function parseUnsafe<I, O>(
+  usp: URLSearchParams,
+  schema: S.Schema<I, O>,
+) {
+  return S.parseSync(schema)(parse_(usp));
+}
+
+function parse_(usp: URLSearchParams) {
+  const params: Record<string, string | null | string[]> = {};
 
   for (const key of usp.keys()) {
     if (key.endsWith("[]")) {
-      params[key.replace("[]", "")] = usp.getAll(key).map(parser);
+      params[key.replace("[]", "")] = usp.getAll(key);
     } else {
-      params[key] = parser(usp.get(key));
+      params[key] = usp.get(key);
     }
   }
 
